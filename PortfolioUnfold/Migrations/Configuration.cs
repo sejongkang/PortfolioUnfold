d878b9235334c1700722eabbb3c3ab98ccb9f453
SejongKang <sejong055@gmail.com>
2020-05-31 16:21:52 +0900

    Arrangement

--- PortfolioUnfold/Migrations/Configuration.cs
+++ PortfolioUnfold/Migrations/Configuration.cs
@@ -5,7 +5,7 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<PortfolioUnfold.Models.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.DataContext>
     {
         public Configuration()
         {
